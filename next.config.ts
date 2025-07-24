8f0321b2c19467d37ba8d92707158835a9f9eb1e
unknown <rahulthesun82@gmail.com>
2025-07-24 13:12:36 +0530

    Next Export of Static Sites

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  output: 'export', // 👈 Enables static HTML export
+  images: {
+    unoptimized: true, // 👈 Required for static export if using <Image />
+  },
 };
 
 export default nextConfig;
